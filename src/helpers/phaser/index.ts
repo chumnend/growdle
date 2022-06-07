9dd65b7a3191819812ad05cc0dfd9975535bd2e8
Nicholas Chumney <nicholas.chumney@outlook.com>
2022-06-07 18:56:00 -0400

    remove unused css styles

--- src/helpers/phaser/index.ts
+++ src/helpers/phaser/index.ts
@@ -8,11 +8,13 @@ const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
   parent: 'phaser-container',
   backgroundColor: '#282c34',
-  scale: {
-    mode: Phaser.Scale.ScaleModes.RESIZE,
-    width: window.innerWidth,
-    height: window.innerHeight,
-  },
+  width: 800,
+  height: 600,
+  // scale: {
+  //   mode: Phaser.Scale.ScaleModes.RESIZE,
+  //   width: window.innerWidth,
+  //   height: window.innerHeight,
+  // },
   physics: {
     default: 'arcade',
     arcade: {
